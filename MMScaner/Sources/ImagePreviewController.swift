6ba9c5405ac59b0066c9073bc2eb2d3cecec9b04
Mei Ma <meimaios@gmail.com>
2017-05-18 00:16:27 +1000

    refactor

--- MMScaner/Sources/ImagePreviewController.swift
+++ MMScaner/Sources/ImagePreviewController.swift
@@ -14,7 +14,6 @@ class ImagePreviewController: UIViewController {
     let detectedQuadrangle: Quadrangle?
     
     @IBOutlet var imageView: UIImageView!
-    var imageViewWidthHeightRatioLayoutConstraint: NSLayoutConstraint?
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -30,15 +29,8 @@ class ImagePreviewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        if let detectedQuadrangle = self.detectedQuadrangle {
-            ImageEditManager.cut(quadrangle: detectedQuadrangle, outOfImageWith: self.imageData, completion: { (image) in
-                self.populate(image: image)
-            })
-        }
-        else {
-            self.populate(image: UIImage(data: self.imageData))
-        }
+        self.preview(imageData: self.imageData,
+                     with: self.detectedQuadrangle)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -52,10 +44,38 @@ class ImagePreviewController: UIViewController {
     }
     
     //MARK: Helper
+    private func preview(imageData: Data,
+                         with detectedQuadrangle: Quadrangle?){
+        if let detectedQuadrangle = detectedQuadrangle {
+            //  show the cropped image
+            ImageEditManager.cut(quadrangle: detectedQuadrangle, outOfImageWith: imageData, completion: { (image) in
+                self.populate(image: image)
+            })
+        }
+        else {
+            //  show the original image
+            self.populate(image: UIImage(data: imageData))
+        }
+        
+    }
+    
+    private func populate(image: UIImage?){
+        guard let image = image else {
+            return
+        }
+        //  adjust the frame of the image view
+        self.adjust(imageView: self.imageView, forImageWith: image.size)
+        
+        //  set the image
+        self.imageView.image = image
+    }
+    
     private func adjust(imageView: UIImageView, forImageWith size: CGSize){
         guard size != .zero else{
             return
         }
+        
+        //  remove the existing width height ratio layout constraint
         if let existingLayoutConstraint = imageView.constraints.first(where: { (layoutConstraint) -> Bool in
             return layoutConstraint.firstItem as? UIImageView == imageView &&
                 layoutConstraint.firstAttribute == .width &&
@@ -65,17 +85,10 @@ class ImagePreviewController: UIViewController {
             imageView.removeConstraint(existingLayoutConstraint)
         }
         let ratio = size.width / size.height
+        
+        //  add a new width height ratio layout constraint that matches the image size
         let newLayoutConstraint = NSLayoutConstraint(item: imageView, attribute: .width, relatedBy: .equal, toItem: imageView, attribute: .height, multiplier: ratio, constant: 1.0)
         imageView.addConstraint(newLayoutConstraint)
         imageView.superview?.layoutIfNeeded()
-    }
-    
-    private func populate(image: UIImage?){
-        guard let image = image else {
-            return
-        }
-        
-        self.adjust(imageView: self.imageView, forImageWith: image.size)
-        self.imageView.image = image
     }
 }
